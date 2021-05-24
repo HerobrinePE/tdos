75f2b4e6f9e500ee8a0724472f9919e36280340a
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2021-05-24 01:07:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,46 +7,68 @@ app.set("view engine", "html");
 app.use(express.urlencoded());
 app.use(express.json());
 let fileInfo = [];
-let y = app.get("/", (request, response) => {
+var songInfo = [];
+console.log(Math.floor(Math.random() * 16777215).toString(16))
+app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 app.post("/ytdl", async (req, res) => {
-  let dir = await req.body;
-  await ytdl.getBasicInfo(dir.ytLink).then(vaw => {
-    const file = `download/${vaw.videoDetails.title}${dir.selection}`;
-    var name = vaw.videoDetails.title;
-    let eInfo = {
-      file: file,
-      link: vaw.videoDetails.embed.iframeUrl,
-      song: `${vaw.videoDetails.title}`
-    };
-    nu(eInfo);
-    app.get("/d", (req, res) => {
-      res.json(eInfo);
-    });
-    if (dir.selection === ".mp3") {
-      ytdl(vaw.videoDetails.videoId, {
-        filter: "audioonly"
-      }).pipe(
-        fs.createWriteStream("download/" + vaw.videoDetails.title + ".mp3")
-      );
-    }
-  });
+  let val = await { link: req.body.ytLink, selection: req.body.selection };
+  loader(val);
 });
-function nu(eInfo) {
-  app.get("/download", async (req, res) => {
-    setTimeout(() => {
-      res.download(eInfo.file, eInfo.name, err => {
-        if (err) throw err;
-        fs.unlink("/app/" + eInfo.file, err => {
-          if (err) throw err;
+async function loader(val) {
+  console.log(val);
+  if (val.selection === ".mp3") {
+    console.log("music");
+    await ytdl.getBasicInfo(val.link).then(vaw => {
+      const file = `download/${vaw.videoDetails.title}${val.selection}`;
+      let eInfo = [
+        file,
+        vaw.videoDetails.embed.iframeUrl,
+        vaw.videoDetails.title,
+        vaw.videoDetails.videoId,
+        vaw.videoDetails.thumbnails[0].url
+      ];
+      fileInfo.push(eInfo);
+      songInfo.push(file);
+    });
+    let yt = await ytdl(fileInfo[0][3], { filter: "audioonly" }).pipe(
+      fs.createWriteStream("download/" + fileInfo[0][2] + ".mp3")
+    );
+    yt.on(
+      "finish",
+      () => {
+        console.log("fin");
+        app.get("/d", (req, res) => {
+          console.log(fileInfo);
+          let data = {
+            song: fileInfo[0][2],
+            link: val.link,
+            thumbnail:fileInfo[0][4]
+          };
+          console.log(data);
+          res.json(data);
         });
-      });
-    }, 2000);
-    fileInfo.splice(0, fileInfo.length);
-  });
+        app.get("/download", async (req, res) => {
+          let file = `/app/${songInfo[0].split("'").join("")}`;
+          res.download(fileInfo[0][0], fileInfo[0][2], err => {
+            if (err) throw err;
+            fs.unlink(file, err => {
+              if (err) throw err;
+            });
+          });
+          fileInfo.splice(0, fileInfo.length);
+          console.log(fileInfo);
+        });
+      },
+      3000
+    );
+  } else {
+    console.log("video");
+    
+    
+  }
 }
-
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
